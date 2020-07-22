3b6e61b5b00575d14a2f4ca132f805f11334865a
Spikey <creativename1111@gmail.com>
2020-07-22 10:51:37 -0400

    attemp to resolve error

--- SpikeyDisEnch/src/Spikey/minecraft/disenchant/Commands.java
+++ SpikeyDisEnch/src/Spikey/minecraft/disenchant/Commands.java
@@ -24,9 +24,9 @@ public class Commands implements CommandExecutor {
 		if (sender instanceof Player) {
 			Player player = (Player) sender;
 			List<VillagerTrade> trades = new ArrayList<>();
-			trades.add(new VillagerTrade(new ItemStack(Material.STONE),new ItemStack(Material.STONE),1));
+			trades.add(new VillagerTrade(new ItemStack(Material.STONE),new ItemStack(Material.COBBLESTONE),1));
 			VillagerInventory inv = new VillagerInventory(trades, player);
-			inv.setName("Disenchant");
+			//inv.setName("Disenchant");
 			inv.open();
 		}
 		return false;
