f92780eb819f5a4318a086eaf4b4898766c95d2a
Spikey <creativename1111@gmail.com>
2020-07-22 12:43:24 -0400

    eliminated if

--- SpikeyDisEnch/src/Spikey/minecraft/disenchant/Commands.java
+++ SpikeyDisEnch/src/Spikey/minecraft/disenchant/Commands.java
@@ -72,11 +72,11 @@ public class Commands implements CommandExecutor {
 			ItemStack[] items = player.getInventory().getStorageContents();
 			
 			for(ItemStack x:items) {
-				if(0 != x.getAmount()) {
+				//if(0 != x.getAmount()) {
 					//trades.add(new VillagerTrade(x,disEnchant(player,x),10));
 					//trades.add(new VillagerTrade(x,x,10));
 					player.sendMessage(x.toString());
-				}
+				//}
 			}
 			
 			
