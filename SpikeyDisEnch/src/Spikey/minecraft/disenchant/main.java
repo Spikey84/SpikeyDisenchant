33375d3436912786339b88b50efa538bac4ee1f5
Spikey <creativename1111@gmail.com>
2020-07-22 10:48:15 -0400

    created test trade inv. Moved method for disenchantment to main.

--- SpikeyDisEnch/src/Spikey/minecraft/disenchant/main.java
+++ SpikeyDisEnch/src/Spikey/minecraft/disenchant/main.java
@@ -1,11 +1,20 @@
 package Spikey.minecraft.disenchant;
 
 
+import java.util.Map;
+
+import javax.annotation.Nonnull;
+
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import net.md_5.bungee.api.ChatColor;
 import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.economy.EconomyResponse;
 
 
 
@@ -51,6 +60,43 @@ public class main extends JavaPlugin {
 	    }
 	    public int costPull() {
 	    	return config.getInt("cost");
+	    }
+	    public ItemStack disEnchant(Player pl, ItemStack in) {
+	    	Player player = pl;
+			//ItemStack enchanted = new ItemStack(player.getInventory().getItemInMainHand());
+	    	ItemStack enchanted = in;
+			int cost = 1000;
+			
+			String prefix = ChatColor.DARK_RED+"["+ChatColor.RED+"Disenchantments"+ChatColor.DARK_RED+"] ";
+			
+			
+			@Nonnull
+			Map<Enchantment, Integer> enchantments = enchanted.getEnchantments();
+			
+			if (enchantments.isEmpty()) {
+				player.sendRawMessage(prefix+ChatColor.WHITE+"Item is not enchanted.");
+				
+			} else {
+				EconomyResponse r = main.econ.withdrawPlayer(player, cost);
+				if(r.transactionSuccess()) {
+					
+					int numEnch = 0;
+					for (Enchantment x : enchantments.keySet()) {
+						enchanted.removeEnchantment(x);
+						numEnch = numEnch+1;
+					}
+					player.sendRawMessage(prefix+ChatColor.WHITE+"Removed "+numEnch+" enchantments.");
+					//player.getInventory().setItemInMainHand(enchanted);	
+				} else {
+					player.sendMessage(prefix+ChatColor.WHITE+"You do not have sufficient funds.");
+				}
+				
+				
+				
+			}
+			return enchanted;
+	    	
+	    	
 	    }
 
 }
