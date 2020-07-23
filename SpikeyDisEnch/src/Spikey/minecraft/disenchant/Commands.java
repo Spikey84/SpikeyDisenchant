099a559961f66cf733e13ec4393fbdc5ee0cd662
Spikey <creativename1111@gmail.com>
2020-07-22 20:21:56 -0400

    Edited

--- SpikeyDisEnch/src/Spikey/minecraft/disenchant/Commands.java
+++ SpikeyDisEnch/src/Spikey/minecraft/disenchant/Commands.java
@@ -73,8 +73,8 @@ public class Commands implements CommandExecutor {
 			for(ItemStack x:items) {
 				if(0 != x.getAmount()) {
 					//trades.add(new VillagerTrade(x,disEnchant(player,x),10));
-					//trades.add(new VillagerTrade(x,x,10));
-					player.sendMessage(x.toString());
+					trades.add(new VillagerTrade(new ItemStack(Material.ANVIL), new ItemStack(Material.GLASS), 10));
+					//player.sendMessage(x.toString());
 				}
 			}
 			
