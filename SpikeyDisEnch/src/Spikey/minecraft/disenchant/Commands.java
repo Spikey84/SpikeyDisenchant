a6657f7559f9280e766e056ffb9e6f7ea63d8674
Spikey <creativename1111@gmail.com>
2020-07-22 21:31:07 -0400

    changed enchant way

--- SpikeyDisEnch/src/Spikey/minecraft/disenchant/Commands.java
+++ SpikeyDisEnch/src/Spikey/minecraft/disenchant/Commands.java
@@ -72,10 +72,12 @@ public class Commands implements CommandExecutor {
 			
 			ItemStack[] items = player.getInventory().getContents();
 			for(int i =0;i<items.length; i++) {
-				if(items[i] != null&&enchanted(items[i]))
-					trades.add(new VillagerTrade(items[i],items[i],10));
+				if(items[i] != null&&enchanted(items[i])) {
+					ItemStack y = items[i];
+					trades.add(new VillagerTrade(items[i],disEnchant(player,y),10));
 					//trades.add(new VillagerTrade(new ItemStack(Material.ANVIL), new ItemStack(Material.GLASS), 10));
 					player.sendMessage("test");
+				}
 				}
 			
 			
