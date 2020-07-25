ccfdac8ebc7d28d2256e63c42dbeb1518ced88f3
Spikey <creativename1111@gmail.com>
2020-07-24 21:02:12 -0400

    Testing github

--- SpikeyDisEnch/src/Spikey/minecraft/disenchant/main.java
+++ SpikeyDisEnch/src/Spikey/minecraft/disenchant/main.java
@@ -20,15 +20,10 @@ import net.milkbowl.vault.economy.EconomyResponse;
 
 public class main extends JavaPlugin {
     FileConfiguration config = getConfig();
-	public static Economy econ = null;
 
 	@Override
 	public void onEnable() {
-        if (!setupEconomy() ) {
-            getLogger().severe(String.format("[%s] - Disabled due to no Vault dependency found!", getDescription().getName()));
-            getServer().getPluginManager().disablePlugin(this);
-            return;
-        }
+
 		getLogger().info("[Test] Enabled");
 		getLogger().info("[Test] © 2020 Spikey");
         this.getCommand("disenchant").setExecutor(new Commands());
@@ -44,20 +39,6 @@ public class main extends JavaPlugin {
 	
 		}
 	
-	 private boolean setupEconomy() {
-	        if (getServer().getPluginManager().getPlugin("Vault") == null) {
-	            return false;
-	        }
-	        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
-	        if (rsp == null) {
-	            return false;
-	        }
-	        econ = rsp.getProvider();
-	        return econ != null;
-	    }
-	    public static Economy getEconomy() {
-	        return econ;
-	    }
 	    public int costPull() {
 	    	return config.getInt("cost");
 	    }
